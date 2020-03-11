dd831d126933fcd30369a4479fdce797b68ba9bd
Fiona Luo <48082672+fiona-luo@users.noreply.github.com>
2020-03-11 00:31:02 -0700

    Update FilesReaderExtraFeaturesAMP.java

--- Descriptor Calculation Programs/FilesReaderExtraFeaturesAMP.java
+++ Descriptor Calculation Programs/FilesReaderExtraFeaturesAMP.java
@@ -1,6 +1,6 @@
 /*
- * This program formats ActiveSequences.txt and InactiveSequences.txt and prints it to TrainingSequences.txt
- * It uses FeatureCalculator to calculate the peptide characteristics
+ * This program adds extra information and features to the active sequences text file for Antimicrobial peptides. 
+ * In general, it's purpose is to format and concatenate files into a readable csv format
  */
 import java.io.*;
 import java.util.*;
@@ -61,61 +61,6 @@ public class FilesReaderExtraFeaturesAMP
         for (int i = 0; i<lengths.length; i++)
         	System.out.println(i + ": " + lengths[i]);
         
-        
-        
-        
-        
-        /*
-        ArrayList<String>[] inactiveSequences = new ArrayList[63]; //store inactive sequences by length (length is index)
-        for (int i = 0; i<inactiveSequences.length; i++) //initialize the array
-        	inactiveSequences[i] = new ArrayList<String>();
-        // this loop formats uniprot_all_data.txt, print the sequences to inactiveSequences.txt
-        line = uniprotbr.readLine(); // skip the first line
-        String s = "";
-        int x = 0;
-        while( x<6072922 && (line = uniprotbr.readLine()) != null ){ //read 6 million sequences
-        	if (line.startsWith(">"))
-        	{
-        		inactiveout.println(s); // print the sequence to inactiveSequences
-        		inactiveSequences[s.length()].add(s);
-        		s = "";
-        		x++;
-        	}
-        	else // it's part of a sequence
-        	{
-        		s += line.trim();
-        	}
-        }
-        for (int i = 0; i< inactiveSequences.length; i++)
-        	System.out.println(i + ": " + inactiveSequences[i].size());
-        inactiveout.close();
-        
-		// then write inactive sequences to TrainingSequences.txt randomly
-        
-        int totalInactiveData = 0;
-        for (int i = 5; i <= 62; i++) // for each specific length
-        {
-        	System.out.println(i);
-        	for (int j = 0; j < lengths[i] && inactiveSequences[i].size()>0; j++) // for the peptides you need of a length
-        	{
-	        	int n = (int)(Math.random()*inactiveSequences[i].size()); // random index
-	        	s = FeatureCalculator.getFeaturesString(inactiveSequences[i].get(n), false); //print to TrainingSequences.txt
-	        	inactiveout2.println(inactiveSequences[i].get(n));
-	        	out.println(s);
-	        	System.out.println(s);
-	        	inactiveSequences[i].remove(n); // take out the used
-	        	totalInactiveData++;
-        	}
-        }
-        System.out.println(totalInactiveData);
-        */
-        
-        
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        // FOR NOW, JUST USE THE FIXED INACTIVE SEQUENCES BC IDK HOW TO CALCULATE HARDNESSES
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        
-        
         while( (line = inactivefixedbr.readLine()) != null ){
         	String hardness = extraFeaturesInactivebr.readLine().trim();
         	if (line.length() >= 5) {
@@ -129,15 +74,3 @@ public class FilesReaderExtraFeaturesAMP
         out.close();
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
